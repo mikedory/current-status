b8a116022b69d54559fece24c1cf5662da24c741
Mike Dory <mike@dory.me>
2016-07-02 13:19:01 -0400

    actually tweeting now`

--- bot.js
+++ bot.js
@@ -1,41 +1,53 @@
-// Our Twitter library
+// load up the libs
 var Twit = require('twit');
+var request = require('request');
 
 // We need to include our configuration file
 var T = new Twit(require('./config/config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+function getRandomGif() {
+	request.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=current+status', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	gifJSON = JSON.parse(body);
+	    gifURL = (gifJSON.data.image_url) 
+	    tweetGif(gifURL);
+	  }
+	})
+
+	// request
+	//   .get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=current+status', {json:true})
+	//   .on('response', function(response) {
+	//     console.log(response.statusCode) // 200
+	//     console.log(body) // 'image/png'
+	//     gifURL = 'watwat';
+	//     retweetLatest(gifURL);
+
+	//   })
+}
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+function tweetGif(gifURL) {
+
+	console.log(gifURL);
+
+	tweetString = "current status: " + gifURL;
+
+	T.post('statuses/update', { status: tweetString }, function(err, data, response) {
 			if (response) {
 				console.log('Success! Check your bot, it should have retweeted something.')
 			}
 			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
+			if (err) {
 				console.log('There was an error with Twitter:', error);
 			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+	})
+
 }
 
-// Try to retweet something as soon as we run the program...
-retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+// Get dat gif doe
+getRandomGif();
+
+
+// ...and then every ten minutes after that. Time here is in milliseconds, so
+// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 10 = 10 minutes
+setInterval(retweetLatest, 1000 * 60 * 10);
