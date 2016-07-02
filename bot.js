71c9a18f272d5e94aeb16177f59a082cacac688f
Mike Dory <mike@dory.me>
2016-07-02 13:42:22 -0400

    adding node-base64-image, and making it actually work

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
 // load up the libs
 var Twit = require('twit');
 var request = require('request');
+var base64 = require('node-base64-image');
 
 // We need to include our configuration file
 var T = new Twit(require('./config/config.js'));
+
 
 function getRandomGif() {
 	request.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=current+status', function (error, response, body) {
@@ -32,15 +34,38 @@ function tweetGif(gifURL) {
 
 	tweetString = "current status: " + gifURL;
 
-	T.post('statuses/update', { status: tweetString }, function(err, data, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (err) {
-				console.log('There was an error with Twitter:', error);
-			}
-	})
+	// T.post('statuses/update', { status: tweetString }, function(err, data, response) {
+	// 		if (response) {
+	// 			console.log('Success! Check your bot, it should have retweeted something.')
+	// 		}
+	// 		// If there was an error with our Twitter call, we print it out here.
+	// 		if (err) {
+	// 			console.log('There was an error with Twitter:', err);
+	// 		}
+	// })
+
+
+
+	profileImageURL = 'http://s3.amazonaws.com/giphygifs/media/Ggjwvmqktuvf2/100w_s.gif';
+	var options = {string: true};
+
+	base64.encode(profileImageURL, options, function (err, image) {
+    if (err) {
+        console.log(err);
+    }
+		T.post('account/update_profile_image', { image: image }, function(err, data, response) {
+				if (response) {
+					console.log('Should have updated the image')
+				}
+				// If there was an error with our Twitter call, we print it out here.
+				if (err) {
+					console.log('There was an error with Twitter:', err);
+				}
+		});
+
+    console.log(image);
+  });
+
 
 }
 
@@ -50,4 +75,4 @@ getRandomGif();
 
 // ...and then every ten minutes after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 10 = 10 minutes
-setInterval(retweetLatest, 1000 * 60 * 10);
+// setInterval(getRandomGif, 1000 * 60 * 10);
